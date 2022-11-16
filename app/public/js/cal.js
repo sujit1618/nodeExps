e8ce82cee1968dbaf75e88e55e313bda330d4a25
Sujit <sjoshi1@saic.edu>
2022-11-17 02:07:14 +0530

    addded qty and inks functionalities

--- app/public/js/cal.js
+++ app/public/js/cal.js
@@ -130,12 +130,496 @@ let calMenu = {
     }
 };
 
+let menu = {
+    25: {
+        "name": "25-कालदर्शिका",
+        6: {
+            250: {
+                "baseRate": 22,
+                "extraRate": 5.75
+            },
+            500: {
+                "baseRate": 19.25,
+                "extraRate": 3.25
+            },
+            1000: {
+                "baseRate": 17.75,
+                "extraRate": 2
+            }
+        }
+    },
+    31: {
+        "name": "31-उमंग",
+        6: {
+            250: {
+                "baseRate": 22,
+                "extraRate": 5.75
+            },
+            500: {
+                "baseRate": 19.25,
+                "extraRate": 3.25
+            },
+            1000: {
+                "baseRate": 17.75,
+                "extraRate": 2
+            }
+        }
+    },
+    35: {
+        "name": "35-Tarang",
+        6: {
+            250: {
+                "baseRate": 23,
+                "extraRate": 5.75
+            },
+            500: {
+                "baseRate": 20.25,
+                "extraRate": 3.25
+            },
+            1000: {
+                "baseRate": 18.75,
+                "extraRate": 2
+            }
+        }
+    },
+    37: {
+        "name": "37-Sarang",
+        6: {
+            250: {
+                "baseRate": 23,
+                "extraRate": 5.75
+            },
+            500: {
+                "baseRate": 20.25,
+                "extraRate": 3.25
+            },
+            1000: {
+                "baseRate": 18.75,
+                "extraRate": 2
+            }
+        }
+    },
+    51: {
+        "name": "51-नवनिर्णय",
+        6: {
+            250: {
+                "baseRate": 22,
+                "extraRate": 6.5
+            },
+            500: {
+                "baseRate": 26.25,
+                "extraRate": 3.75
+            },
+            1000: {
+                "baseRate": 25,
+                "extraRate": 2.65
+            }
+        }
+    },
+    55: {
+        "name": "55-Stride",
+        6: {
+            250: {
+                "baseRate": 29,
+                "extraRate": 6.5
+            },
+            500: {
+                "baseRate": 26.25,
+                "extraRate": 3.75
+            },
+            1000: {
+                "baseRate": 25,
+                "extraRate": 2.65
+            }
+        }
+    },
+    111: {
+        "name": "111-Grace",
+        12: {
+            100: {
+                "baseRate": 46.5,
+                "extraRate": 13.5
+            },
+            250: {
+                "baseRate": 39,
+                "extraRate": 6.5
+            },
+            500: {
+                "baseRate": 36.25,
+                "extraRate": 3.75
+            },
+            1000: {
+                "baseRate": 35,
+                "extraRate": 2.6
+            }
+        }
+    },
+    141: {
+        "name": "141-Bright",
+        12: {
+            100: {
+                "baseRate": 46.5,
+                "extraRate": 13.5
+            },
+            250: {
+                "baseRate": 39,
+                "extraRate": 6.5
+            },
+            500: {
+                "baseRate": 36.25,
+                "extraRate": 3.75
+            },
+            1000: {
+                "baseRate": 35,
+                "extraRate": 2.6
+            }
+        }
+    },
+    161: {
+        "name": "161-Smash",
+        6: {
+            250: {
+                "baseRate": 38,
+                "extraRate": 7.5
+            },
+            500: {
+                "baseRate": 35.75,
+                "extraRate": 5.5
+            },
+            1000: {
+                "baseRate": 34,
+                "extraRate": 3.6
+            }
+        },
+        12: {
+            250: {
+                "baseRate": 51.5,
+                "extraRate": 7.25
+            },
+            500: {
+                "baseRate": 49.25,
+                "extraRate": 5.25
+            },
+            1000: {
+                "baseRate": 47.5,
+                "extraRate": 3.6
+            }
+        }
+    },
+    81: {
+        "name": "81-Spark",
+        6: {
+            250: {
+                "baseRate": 28,
+                "extraRate": 6.5
+            },
+            500: {
+                "baseRate": 25,
+                "extraRate": 3.75
+            },
+            1000: {
+                "baseRate": 24,
+                "extraRate": 2.6
+            }
+        }
+    },
+    171: {
+        "name": "171-Majestic",
+        6: {
+            250: {
+                "baseRate": 48,
+                "extraRate": 7.5
+            },
+            500: {
+                "baseRate": 45.75,
+                "extraRate": 5.5
+            },
+            1000: {
+                "baseRate": 44,
+                "extraRate": 3.6
+            }
+        },
+        12: {
+            100: {
+                "baseRate": 72.5,
+                "extraRate": 13.5
+            },
+            250: {
+                "baseRate": 66,
+                "extraRate": 7.25
+            },
+            500: {
+                "baseRate": 63.75,
+                "extraRate": 5.25
+            },
+            1000: {
+                "baseRate": 62,
+                "extraRate": 3.6
+            }
+        }
+    },
+    181: {
+        "name": "181-Classic",
+        6: {
+            250: {
+                "baseRate": 57,
+                "extraRate": 7.5
+            },
+            500: {
+                "baseRate": 54.75,
+                "extraRate": 5.5
+            },
+            1000: {
+                "baseRate": 53,
+                "extraRate": 3.6
+            }
+        },
+        12: {
+            100: {
+                "baseRate": 81.5,
+                "extraRate": 13.5
+            },
+            250: {
+                "baseRate": 75,
+                "extraRate": 7.25
+            },
+            500: {
+                "baseRate": 72.75,
+                "extraRate": 5.25
+            },
+            1000: {
+                "baseRate": 71,
+                "extraRate": 3.6
+            }
+        }
+    },
+    131: {
+        "name": "131-Serene",
+        6: {
+            100: {
+                "baseRate": 46.5,
+                "extraRate": 13.5
+            },
+            250: {
+                "baseRate": 39,
+                "extraRate": 6.5
+            },
+            500: {
+                "baseRate": 36,
+                "extraRate": 3.75
+            },
+            1000: {
+                "baseRate": 35,
+                "extraRate": 2.6
+            }
+        }
+    },
+    91: {
+        "name": "91-Glimpse",
+        4: {
+            250: {
+                "baseRate": 31.75,
+                "extraRate": 5.5
+            },
+            500: {
+                "baseRate": 29.25,
+                "extraRate": 3.25
+            },
+            1000: {
+                "baseRate": 28.25,
+                "extraRate": 2.25
+            }
+        }
+    },
+    27: {
+        "name": "27-जीवन अमृत",
+        6: {
+            150: {
+                "baseRate": 27,
+                "extraRate": 10
+            },
+            250: {
+                "baseRate": 23,
+                "extraRate": 5.75
+            },
+            500: {
+                "baseRate": 20.25,
+                "extraRate": 3.25
+            },
+            1000: {
+                "baseRate": 18.75,
+                "extraRate": 2
+            }
+        }
+    },
+    17: {
+        "name": "17-Seasons",
+        6: {
+            500: {
+                "baseRate": 12,
+                "extraRate": 2.8
+            },
+            1000: {
+                "baseRate": 10.5,
+                "extraRate": 1.4
+            }
+        }
+    }
+};
+let pageOptionsSel = 6;
+let inksSel = 1;
+let calID;
+
+let unHover = "background-color: #f0f0f0;";
+let hoverStyle = " background: rgb(214,248,255); background: -moz-linear-gradient(180deg, rgba(214,248,255,0.7) 0%, rgba(208,243,250,0.7) 60%, rgba(174,232,245,0.7) 100%); background: -webkit-linear-gradient(180deg, rgba(214,248,255,0.7) 0%, rgba(208,243,250,0.7) 60%, rgba(174,232,245,0.7) 100%); background: linear-gradient(180deg, rgba(214,248,255,0.7) 0%, rgba(208,243,250,0.7) 60%, rgba(174,232,245,0.7) 100%); filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#d6f8ff',endColorstr='#aee8f5',GradientType=1); ";
+let minQty;
 function callCalID() {
     const queryString = window.location.search;
     console.log(queryString);
     const urlParams = new URLSearchParams(queryString);
-
-    const calID = JSON.parse(urlParams.get('calID'));
+    calID = JSON.parse(urlParams.get('calID'));
+    console.log(urlParams.get('calID'));
     console.log(calID);
-    document.getElementById('calNum').innerHTML="You chose " + calMenu[calID].name;
+    // document.getElementById('calNum').innerHTML="You chose " + calMenu[calID].name;
+    document.getElementById('calendar-name').innerHTML = calMenu[calID].name;
+    document.getElementById('cal-img').setAttribute('src', '/image/calendarpngfiles/' + calID + '.png');
+    document.getElementById('description').innerHTML = calMenu[calID].description;
+
+    document.getElementById('sheets-6').style.display = 'none';
+    document.getElementById('sheets-12').style.display = 'none';
+    document.getElementById('sheets-4').style.display = 'none';
+
+    if (menu[calID][6] != null) { //6 sheets option available
+        pageOptionsSel = 6;
+        document.getElementById('sheets-6').style = hoverStyle;
+    }
+    if (menu[calID][12] != null) { // 12 sheets option
+        pageOptionsSel = 12;
+        document.getElementById('sheets-12').style = hoverStyle;
+    }
+    if (menu[calID][12] != null && menu[calID][6] != null) { // 12 sheets option
+        document.getElementById('sheets-12').style = hoverStyle;
+        document.getElementById('sheets-6').style = unHover;
+        pageOptionsSel = 12;
+    }
+    if (menu[calID][4] != null) { //4 sheets option
+        document.getElementById('sheets-4').style = hoverStyle;
+        pageOptionsSel = 4;
+    }
+    pageOptionSelection();
+
+    if (calID == 171 || calID == 181) {
+        document.getElementById('cal-qty').setAttribute('value', '100');
+        document.getElementById('cal-qty').setAttribute('min', '100');
+        document.getElementById('total-for-qty').innerHTML = "including GST, <br> for " + '100' + " copies";
+        document.getElementById('min-qty-h3').innerHTML = "Enter quantity (min. " + '100' + ")";
+        minQty = 100;
+    } else {
+        document.getElementById('cal-qty').setAttribute('value', JSON.parse(calMenu[calID].toqty));
+        document.getElementById('cal-qty').setAttribute('min', JSON.parse(calMenu[calID].toqty));
+        document.getElementById('total-for-qty').innerHTML = "including GST, <br> for " + (calMenu[calID].toqty) + " copies";
+        document.getElementById('min-qty-h3').innerHTML = "Enter quantity (min. " + (calMenu[calID].toqty) + ")";
+        minQty = JSON.parse(calMenu[calID].toqty);
+    }
+
+    inksSelection(1);
+
+    // document.getElementById('cal-qty').onkeyup= function(){
+        // if ( document.getElementById('cal-qty').getAttribute('value') != minQty) {
+            // document.getElementById('qty-error').style.display= 'block';
+            // document.getElementById('cal-qty').style.backgroundColor = 'rgb(255, 192, 192)';
+        // }
+        // else{
+            // document.getElementById('qty-error').style.display= 'none';
+            // document.getElementById('cal-qty').style.backgroundColor = '#fff';
+        // }
+        // console.log('one key up');
+    // };
 }
+
+function pageSel(option) {
+    pageOptionsSel = option;
+    if (menu[calID][4] != null) {
+        document.getElementById('sheets-4').style = unHover;
+    }
+    if (menu[calID][6] != null) {
+        document.getElementById('sheets-6').style = unHover;
+    }
+    if (menu[calID][12] != null) {
+        document.getElementById('sheets-12').style = unHover;
+    }
+
+    pageOptionSelection();
+
+    document.getElementById('sheets-' + pageOptionsSel).style = hoverStyle;
+
+    if (calID == 171 || calID == 181) {
+        if (option == 6) {
+            document.getElementById('cal-qty').setAttribute('value', '250');
+            document.getElementById('cal-qty').setAttribute('min', '250');
+            document.getElementById('total-for-qty').innerHTML = "including GST, <br> for " + '250' + " copies";
+            document.getElementById('min-qty-h3').innerHTML = "Enter quantity (min. " + '250' + ")";
+            minQty = 250;
+        }
+        if (option == 12) {
+            document.getElementById('cal-qty').setAttribute('value', '100');
+            document.getElementById('cal-qty').setAttribute('min', '100');
+            document.getElementById('total-for-qty').innerHTML = "including GST, <br> for " + '100' + " copies";
+            document.getElementById('min-qty-h3').innerHTML = "Enter quantity (min. " + '100' + ")";
+            minQty = 100;
+        }
+    }
+
+}
+
+function pageOptionSelection() {
+    if (pageOptionsSel == 6) {
+        document.getElementById('rate-table-h3').innerHTML = "Single Sided (6 Sheets) calendar bulk prices";
+    }
+    if (pageOptionsSel == 12) {
+        document.getElementById('rate-table-h3').innerHTML = "Double Sided (12 Sheets) calendar bulk prices";
+    }
+    if (pageOptionsSel == 4) {
+        document.getElementById('rate-table-h3').innerHTML = "Single Sided (4 Sheets) calendar bulk prices";
+    }
+
+    document.getElementById('4-sheets-table').style.display = 'none';
+    document.getElementById('6-sheets-table').style.display = 'none';
+    document.getElementById('12-sheets-table').style.display = 'none';
+    document.getElementById(pageOptionsSel + '-sheets-table').style.display = 'block';
+
+
+    for (let i = 1; i <= 1000; i++) {
+        if (menu[calID][pageOptionsSel][i] != null) {
+            document.getElementById('base-' + pageOptionsSel + '-' + i).innerHTML = Math.round((menu[calID][pageOptionsSel][i].baseRate * 1.18 + Number.EPSILON) * 100) / 100; //Math.round((roundAmount + roundGst + Number.EPSILON) * 100) / 100;
+            document.getElementById('extra-' + pageOptionsSel + '-' + i).innerHTML = Math.round((menu[calID][pageOptionsSel][i].extraRate * 1.18 + Number.EPSILON) * 100) / 100; //Math.round((roundAmount + roundGst + Number.EPSILON) * 100) / 100;
+        }
+    }
+    for (let i = 0; i <= 4; i++) {
+        let pageSelArray = [100, 150, 250, 500, 1000];
+        if (menu[calID][pageOptionsSel][pageSelArray[i]] == null) {
+            document.getElementById('table-' + pageOptionsSel + '-' + pageSelArray[i]).style.display = 'none';
+        }
+    }
+
+}
+
+function inksSelection(x) {
+    inksSel = x;
+    document.getElementById('inks-1').style = unHover;
+    document.getElementById('inks-2').style = unHover;
+    document.getElementById('inks-3').style = unHover;
+    document.getElementById('inks-'+inksSel).style = hoverStyle;
+}
+
+// function runQtyCheck() {
+    // if ( document.getElementById('cal-qty').getAttribute('value') != minQty) {
+        // document.getElementById('qty-error').style.display= 'block';
+        // document.getElementById('cal-qty').style.backgroundColor = 'rgb(255, 192, 192)';
+    // }
+    // else{
+        // document.getElementById('qty-error').style.display= 'none';
+        // document.getElementById('cal-qty').style.backgroundColor = '#fff';
+    // }
+    // console.log('one key up');
+// }
